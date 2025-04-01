21287f7b93a3e519db142e81828a1a4e90c27e22
jb-garcia <josebenedicgarcia2@gmail.com>
2025-04-01 20:29:19 +0800

    Update server.js

--- server.js
+++ server.js
@@ -13,6 +13,9 @@ dotenv.config();
 
 const app = express();
 app.use(cors());
+app.use(cors({
+    origin: 'https://moodapp-pflu193ie-jose-benedic-garcias-projects.vercel.app'
+  }));
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 
